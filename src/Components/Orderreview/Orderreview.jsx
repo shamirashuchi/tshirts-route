5ed0d46bde6d94c62e4dd534e48109dabd0745b3
shamirashuchi <shamirashuchi@gmail.com>
2023-04-07 22:13:25 +0600

    first commit

---
+++ src/Components/Orderreview/Orderreview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orderreview = () => {
+    return (
+        <div>
+            <h3>This is orderreview cpmponent</h3>
+        </div>
+    );
+};
+
+export default Orderreview;
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Main from './Components/Layout/Main';
+import Orderreview from './Components/Orderreview/Orderreview';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>,
+        loader: () => fetch('Tshirts.json')
+      },
+      {
+        path:'review',
+        element:<Orderreview></Orderreview>
+      }
+    ]
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
